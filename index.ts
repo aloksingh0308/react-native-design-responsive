948a27e2ce5912cce98b8a36cbb9675196e0d167
Alok Singh <alok@myyogateacher.com>
2024-09-13 00:42:09 +0530

    update readme file

--- index.ts
+++ index.ts
@@ -11,17 +11,17 @@ const { width: SCREEN_WIDTH } = Dimensions.get('window');
 // It is based on the screen width of your design layouts e.g Height 600 x Width 375
 const scale = SCREEN_WIDTH / 375;
 
-export function normalize(size) {
+export function normalize(size:number) {
 return PixelRatio.roundToNearestPixel(size * scale);
 }
 
-export const vw = (width) => {
+export const vw = (width:number) => {
 const percent = (width / DesignWidth) * 100;
 const elemWidth = parseFloat(`${percent}%`);
 return PixelRatio.roundToNearestPixel(screenWidth * elemWidth / 100);
 };
 
-export const vh = (height) => {
+export const vh = (height:number) => {
 const percent = (height / DesignHeight) * 100;
 const elemHeight = parseFloat(`${percent}%`);
 return PixelRatio.roundToNearestPixel(screenHeight * elemHeight / 100);
